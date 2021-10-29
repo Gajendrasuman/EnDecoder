c6708f2b77ea46cfff7623ccbe80f6821c98468c
Gajendrasuman <41474933+Gajendrasuman@users.noreply.github.com>
2021-10-29 12:05:26 +0530

    Add files via upload

---
+++ setup.py.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '2.0.2'
+DESCRIPTION = 'This package is used to encrypt or decrypt the data'
+
+# Setting up
+setup(
+    name="EnDecoder",
+    version=VERSION,
+    author="GS Tech",
+    author_email="<Gajendrasuman868@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['encrypt','decrypt'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
